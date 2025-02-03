ccdd1d94c498ea1103906cdc9f901d053bfd6667
Manuel de la paz <emdelapazt@gmail.com>
2025-02-02 21:07:48 -0500

    Creacion de modulos

--- isyel/settings.py
+++ isyel/settings.py
@@ -117,6 +117,13 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = BASE_DIR /"static"
+
+STATICFILES_DIRS = [
+    "isyel/static"
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
